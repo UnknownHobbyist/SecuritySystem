2c774b0b50ef4b026a05b34a26f125da874e478e
UnknownHobbyist <nakeltohd@gmail.com>
2019-10-25 21:37:17 +0200

    Edited system.py

--- state.enum.py
+++ state.enum.py
@@ -1,10 +1,16 @@
 from enum import Enum
 
+#
+# unused
+#
 class AuthID(Enum):
     RFID = 0
     PWD  = 1
 
+#
+# Gives the State of the Alarm
+#
 class AlarmState(Enum):
     ARMED = 0
     RUNNING  = 1
-    DEACTIVE = 2
+    DISABLED = 2
