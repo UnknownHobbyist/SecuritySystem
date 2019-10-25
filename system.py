a16c9b811d747e3d73eb107e72f1516188136384
UnknownHobbyist <nakeltohd@gmail.com>
2019-10-25 21:46:47 +0200

    testing

--- system.py
+++ system.py
@@ -1,17 +1,32 @@
 import json
 import auth.enum.py
+
 from json_service import JSONService
 
+#gpio imports
+#will be removed later
+from gpiozero import TonalBuzzer
+from gpiozero.tones import Tone
+from gpiozero.tones import Tone
+from gpiozero import LED
+from time import sleep
 
 class SecuritySystem:
     # if the alarm is armed or not
-    alarmState =
+    alarmState = AlarmState.DISABLED
+
+    #initializing sound for triggerAlarm
+    button_sounds = {
+        Button(2): Sound("samples/drum_tom_mid_hard.wav"),
+        Button(3): Sound("samples/drum_cymbal_open.wav"),
+    }
+
 
     #
     # Changes the state of the alarm
     #
     def changeAlarm():
-        alarmArmed = !alarmArmed
+        alarmState = !alarmArmed
 
     #
     # authType {"rfid", "pwd"}
@@ -23,3 +38,22 @@ class SecuritySystem:
             return True
         else:
             return False
+
+    def triggerAlarm():
+
+        #
+        # Multi-Threading for sound and light
+        # testing
+        #
+
+        #light
+        led = LED(17)
+        led.on()
+        sleep(1)
+        led.off();
+
+        #sound
+        soundBox = TonalBuzzer(17)
+        soundBox.play(Tone(60))
+        sleep(1)
+        soundBox.stop()
