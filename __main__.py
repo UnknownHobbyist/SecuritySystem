f03bcf9ba8a5d38cb18ee0b752ac01ef33ef4e92
Sners03 <reichelarslukas@gmail.com>
2019-10-25 21:50:44 +0200

    minor changes

--- __main__.py
+++ __main__.py
@@ -1,5 +1,8 @@
 import RPi.GPIO as GPIO
+import settings
 import system
 sec_serv = system.SystemService()
 if __name__ == '__main__':
     GPIO.setmode(GPIO.BOARD)
+
+    GPIO.setup(GPIO_SETIINGS["ALARM_CHANGER"])
--- gpio_service.py
+++ gpio_service.py
@@ -1,2 +1,9 @@
 import time
 import RPi.GPIO as GPIO
+import __main__
+import state.enum as se
+import settings
+
+def gpio_alarm_LEDs():
+    while __main__.sec_serv.alarmState == se.AlarmState.RUNNING:
+        
