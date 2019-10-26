dcbde81eea0ca3d8297a1cb4b7dd445e3193010e
UnknownHobbyist <nakeltohd@gmail.com>
2019-10-26 14:18:40 +0200

    fix

--- __main__.py
+++ __main__.py
@@ -11,10 +11,10 @@ if __name__ == '__main__':
     GPIO.setmode(GPIO.BOARD)
 
     #setup for Output Pins
-    GPIO.setup(GPIO_SETIINGS["ALARM_CHANGER"], GPIO.OUT)
+    GPIO.setup(GPIO_SETTINGS["ALARM_CHANGER"], GPIO.OUT)
 
     #setup for Input Pins
-    GPIO.setup(GPIO_SETIINGS["ALARM_SIGNAL"]["1"], GPIO.IN,  pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(GPIO_SETTINGS["ALARM_SIGNAL"]["1"], GPIO.IN,  pull_up_down=GPIO.PUD_DOWN)
 
     #sec_serv = system.SystemService()
 
