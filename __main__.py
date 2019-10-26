0d26b71fb3b6335d36abc59cc14e50f0826311f4
UnknownHobbyist <nakeltohd@gmail.com>
2019-10-26 14:10:07 +0200

    fix

--- __main__.py
+++ __main__.py
@@ -24,3 +24,13 @@ if __name__ == '__main__':
     num_pad_checker = threading.Thread(target=num_pad.check)
     num_pad_checker.start()
     num_pad_checker.join()
+
+if __name__ == 'num_pad'
+    #use RPi.GPIO Layout
+    GPIO.setmode(GPIO.BOARD)
+
+    #setup for Output Pins
+    GPIO.setup(GPIO_SETIINGS["ALARM_CHANGER"], GPIO.OUT)
+
+    #setup for Input Pins
+    GPIO.setup(GPIO_SETIINGS["ALARM_SIGNAL"]["1"], GPIO.IN,  pull_up_down=GPIO.PUD_DOWN)
