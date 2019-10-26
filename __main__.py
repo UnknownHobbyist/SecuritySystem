899a20ca5265f90fa78ac6f594da30564514ea8a
UnknownHobbyist <nakeltohd@gmail.com>
2019-10-26 14:02:07 +0200

    added num pad

--- __main__.py
+++ __main__.py
@@ -5,6 +5,7 @@ sec_serv = None
 
 if __name__ == '__main__':
     import system
+    from num_pad
 
     #use RPi.GPIO Layout
     GPIO.setmode(GPIO.BOARD)
@@ -15,4 +16,11 @@ if __name__ == '__main__':
     #setup for Input Pins
     GPIO.setup(GPIO_SETIINGS["ALARM_SIGNAL"]["1"], GPIO.IN,  pull_up_down=GPIO.PUD_DOWN)
 
-    sec_serv = system.SystemService()
+    #sec_serv = system.SystemService()
+
+
+    # checks if someone inputs something over the membrane pad
+    num_pad = num_pad.NumPad();
+    num_pad_checker = threading.Thread(target=num_pad.check)
+    num_pad_checker.start()
+    num_pad_checker.join()
