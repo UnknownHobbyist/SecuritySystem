a989902e94ce337086b85166e8ff16ecb329d7ff
Sners03 <reichelarslukas@gmail.com>
2019-10-26 18:00:25 +0200

    rfid final

--- rfid_read.py
+++ rfid_read.py
@@ -9,22 +9,25 @@ def runWhileRFID():
     sys.path.append('/home/pi/MFRC522-python')
 
     from __main__ import sec_serv
-    
+
     from mfrc522 import SimpleMFRC522
     reader = SimpleMFRC522()
 
     while True:
-        print(2)
-        id, text = reader.read()
-        print(3)
-        if sec_serv.auth(str(id),"rfid")==True:
-            if sec_serv.alarmState != AlarmState.DISABLED:
-                sec_serv.changeAlarm(AlarmState.DISABLED)
+        try:
+            print(2)
+            id, text = reader.read()
+            print(id)
+            if sec_serv.auth(str(id),"rfid")==True:
+                if sec_serv.alarmState != AlarmState.DISABLED:
+                    sec_serv.changeAlarm(AlarmState.DISABLED)
+                else:
+                    if sec_serv.alarmState == AlarmState.RUNNING:
+                        sec_serv.stopAlarm()
+                    sec_serv.changeAlarm(AlarmState.ARMED)
+                print("Security system "+str(sec_serv.alarmState))
             else:
-                if sec_serv.alarmState == AlarmState.RUNNING:
-                    sec_serv.stopAlarm()
-                sec_serv.changeAlarm(AlarmState.ARMED)
-            print("Security system "+str(sec_serv.alarmState))
-        else:
-            print("access denied")
-        time.sleep(2)
+                print("access denied")
+            time.sleep(2)
+        finally:
+            pass
