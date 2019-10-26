cf3fb96aebbcf4ce54b5019f17730f69ceca8c46
Sners03 <reichelarslukas@gmail.com>
2019-10-26 10:16:10 +0200

    small changes

--- __main__.py
+++ __main__.py
@@ -3,7 +3,11 @@ import settings
 import system
 sec_serv = system.SystemService()
 if __name__ == '__main__':
+    #use RPi.GPIO Layout
     GPIO.setmode(GPIO.BOARD)
 
+    #setup for Output Pins
     GPIO.setup(GPIO_SETIINGS["ALARM_CHANGER"], GPIO.OUT)
-    GPIO.setup(GPIO_SETIINGS["ALARM_SIGNAL"], GPIO.IN)
+
+    #setup for Input Pins
+    GPIO.setup(GPIO_SETIINGS["ALARM_SIGNAL"]["1"], GPIO.IN)
--- gpio_service.py
+++ gpio_service.py
@@ -4,9 +4,15 @@ import __main__
 import state.enum as se
 import settings
 
-def gpio_alarm_LEDs():
+#Activating Alarm LEDs
+def Gpio_Alarm_LEDs():
     while __main__.sec_serv.alarmState == se.AlarmState.RUNNING:
         GPIO.output(GPIO_SETIINGS["ALARM_CHANGER"], GPIO.HIGH)
         time.sleep(0.5)
         GPIO.output(GPIO_SETIINGS["ALARM_CHANGER"], GPIO.LOW)
         time.sleep(0.5)
+
+def Handle_Alarm_Signal():
+    while True:
+        if GPIO.input(GPIO_SETIINGS["ALARM_SIGNAL"]["1"]) == GPIO.HIGH:
+            __main__.sec_serv.triggerAlarm()
