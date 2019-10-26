54d912b3ac43cac026d3f40a1974b2ebe8a59217
UnknownHobbyist <nakeltohd@gmail.com>
2019-10-26 15:20:07 +0200

    fix

--- gpio_service.py
+++ gpio_service.py
@@ -7,14 +7,14 @@ from settings import *
 #Activating Alarm LEDs
 def gpioAlarmLEDs():
     while __main__.sec_serv.alarmState == se.AlarmState.RUNNING:
-        GPIO.output(GPIO_SETIINGS["ALARM_CHANGER"], GPIO.HIGH)
+        GPIO.output(GPIO_SETTINGS["ALARM_CHANGER"], GPIO.HIGH)
         time.sleep(0.5)
-        GPIO.output(GPIO_SETIINGS["ALARM_CHANGER"], GPIO.LOW)
+        GPIO.output(GPIO_SETTINGS["ALARM_CHANGER"], GPIO.LOW)
         time.sleep(0.5)
 
 def handleAlarmSignal():
     time.sleep(1.5)
-    if GPIO.input(GPIO_SETIINGS["ALARM_SIGNAL"]["1"]) == GPIO.HIGH:
+    if GPIO.input(GPIO_SETTINGS["ALARM_SIGNAL"]["1"]) == GPIO.HIGH:
             __main__.sec_serv.triggerAlarm()
 
 GPIO.setmode(GPIO.BOARD);
