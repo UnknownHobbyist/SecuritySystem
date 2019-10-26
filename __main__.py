240510491c447d1e947b5c892ec9211b6d4308c8
UnknownHobbyist <nakeltohd@gmail.com>
2019-10-26 11:25:26 +0200

    hotfix

--- __main__.py
+++ __main__.py
@@ -1,14 +1,17 @@
 import RPi.GPIO as GPIO
 import settings
-import system
-sec_serv = system.SystemService()
-#use RPi.GPIO Layout
-GPIO.setmode(GPIO.BOARD)
-
-#setup for Output Pins
-GPIO.setup(GPIO_SETIINGS["ALARM_CHANGER"], GPIO.OUT)
-
-#setup for Input Pins
-GPIO.setup(GPIO_SETIINGS["ALARM_SIGNAL"]["1"], GPIO.IN,  pull_up_down=GPIO.PUD_DOWN)
+sec_serv = None
 
 if __name__ == '__main__':
+    import system
+    
+    sec_serv = system.SystemService()
+
+    #use RPi.GPIO Layout
+    GPIO.setmode(GPIO.BOARD)
+
+    #setup for Output Pins
+    GPIO.setup(GPIO_SETIINGS["ALARM_CHANGER"], GPIO.OUT)
+
+    #setup for Input Pins
+    GPIO.setup(GPIO_SETIINGS["ALARM_SIGNAL"]["1"], GPIO.IN,  pull_up_down=GPIO.PUD_DOWN)
