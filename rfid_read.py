b05671ea8b6a260aac0aa8cab4546fbbae5e9444
Sners03 <reichelarslukas@gmail.com>
2019-10-26 16:18:15 +0200

    rfid test

--- rfid_read.py
+++ rfid_read.py
@@ -1,13 +1,14 @@
 import RPi.GPIO as GPIO
 import __main__
 import system
+import time
+from __main__ import sec_serv
 
 import sys
 sys.path.append('/home/pi/MFRC522-python')
 
 from mfrc522 import SimpleMFRC522
 
-sec_serv = system.SecuritySystem()
 reader = SimpleMFRC522()
 
 print("Halten Sie ein Clip oder eine Karte an dem Sensor.")
@@ -16,9 +17,16 @@ while True:
     try:
         id, text = reader.read()
         if sec_serv.auth(str(id),"rfid")==True:
-            print("access granted")
+            if sec_serv.AlarmState != DISABLED
+                sec_serv.changeAlarm(DISABLED)
+            else:
+                sec_serv.changeAlarm(ARMED)
+            print("Security system "+str(sec_serv.AlarmState))
+            time.sleep(2)
         else:
             print("access denied")
+            time.sleep(2)
+
 
     finally:
 
