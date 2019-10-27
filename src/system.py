26b2c6ac8b58b92425cc6a3dfc9974af128bf710
UnknownHobbyist <nakeltohd@gmail.com>
2019-10-27 09:52:35 +0100

    num_pad fixes

--- src/system.py
+++ src/system.py
@@ -49,7 +49,7 @@ class SecuritySystem:
         print("test")
 
     def triggerAlarm(self):
-        if self.alarmState == AlarmState.RUNNING or self.alarmState == AlarmState.DISABLED:
+        if self.alarmState == AlarmState.RUNNING:
             return
 
         self.alarmState = AlarmState.RUNNING
