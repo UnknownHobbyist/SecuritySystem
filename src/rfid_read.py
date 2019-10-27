df41d0a3079c8e05fb2a00fdbbef41050f10c2a2
Sners03 <reichelarslukas@gmail.com>
2019-10-27 09:58:22 +0100

    Reset button

--- src/rfid_read.py
+++ src/rfid_read.py
@@ -15,7 +15,7 @@ def rfid_checker():
     reader = SimpleMFRC522()
 
     while True:
-        sp = subprocess.Popen(['python3.7', '/home/pi/MFRC522-python/Read.py'], stdout=subproces.PIPE, stderr=subprocess.PIPE)
+        sp = subproccess.Popen(['python3.7', '/home/pi/MFRC522-python/Read.py'], stdout=subproces.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = sp.communicate()
         print(stdout)
         time.sleep(2.5)
