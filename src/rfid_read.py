581a6bc25957b89f61319ce564deb111513c6c88
Sners03 <reichelarslukas@gmail.com>
2019-10-27 10:37:53 +0100

    rfid (Fr tho)

--- src/rfid_read.py
+++ src/rfid_read.py
@@ -4,14 +4,21 @@ import subprocess
 import time
 def rfid_named(id):
     from __main__ import sec_serv
+    
+    print(id)
 
     if sec_serv.auth(id,"rfid")==True:
         if sec_serv.alarmState != AlarmState.DISABLED:
             sec_serv.changeAlarm(DISABLED)
+            print("Sicherheitssystem deaktiviert")
+
             if sec_serv.alarmState == AlarmState.RUNNING:
                 sec_serv.stopAlarm()
+                print("Alarm deaktiviert")
+
         else:
             sec_serv.changeAlarm(ARMED)
+            print("Sicherheitssystem aktiviert")
 
 def rfid_checker(callback_function):
 
